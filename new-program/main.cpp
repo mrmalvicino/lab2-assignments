e9c86ef82fde6bab3f2148b195e9023c2d18330a
mrmalvicino <mrmalvicino@gmail.com>
2023-08-17 16:42:05 -0300

    Windows build tasks.json

--- new-program/main.cpp
+++ new-program/main.cpp
@@ -2,6 +2,10 @@
 #include "functions.h"
 
 int main(){
+    int var1 = 3;
+    int var2;
+    std::cin >> var2;
+    var1 = var2;
     hello_world();
 }
 
@@ -9,8 +13,9 @@ int main(){
 COMPILAR:
 g++ -o ./bin/main main.cpp functions.cpp
 
-EJECUTAR
+EJECUTAR:
 ./bin/main
+./bin/main.exe
 
 CORRER PRUEBA DE ESCRITORIO:
 ./bin/main < ./test.txt
